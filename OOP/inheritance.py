206d046c55bc65766035bcd4183cb71dd826f690
Sabin Shrestha <shresthsabin695@gmail.com>
2026-08-14 17:44:45 +0545

    Add files via upload

---
+++ OOP/inheritance.py
@@ -0,0 +1,28 @@
+class Animal:
+    def __init__(self,name):
+        self.name=name
+        self.is_alive=True
+
+    def eat(self):
+        print(f"{self.name} is eating.")
+
+    def sleep(self):
+        print(f"{self.name} is sleeping.")
+
+class Dog(Animal):
+      pass
+
+class Cat(Animal):
+    pass
+
+
+dog=Dog("Scooby")
+
+cat=Cat("Garfield")
+
+
+dog.eat()
+
+cat.eat()
+
+print(cat.is_alive)
